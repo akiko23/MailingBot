1b16eeb9817af46f4d956ba9fa7fb27e09ee850b
Dmitriy <101889433+akiko23@users.noreply.github.com>
2023-11-05 14:38:09 +0300

    remove bullshit && add a pause when the RPS limit is reached

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 from config import bot, db, ADMIN_ID
 from markups.markups import main_menu as mmenu
 
+TG_RPS_LIMIT = 30
+
 async def make_mailing(data, keyb=None):
     msg_id = data["msg_id"]
-    for uid in filter(lambda x: x != ADMIN_ID, db.get_all_users()):
-        try:
-            await bot.copy_message(chat_id=uid, from_chat_id=ADMIN_ID, message_id=msg_id, reply_markup=keyb)
-        except:
-            continue
+    for r_num, uid in enumerate(filter(lambda x: x != ADMIN_ID, db.get_all_users())):
+        if r_num % TG_RPS_LIMIT == 0:
+            await asyncio.sleep(1)
+        await bot.copy_message(chat_id=uid, from_chat_id=ADMIN_ID, message_id=msg_id, reply_markup=keyb)
     await bot.send_message(ADMIN_ID, "Рассылка завершена", reply_markup=mmenu)
